e76651c0de02751fe7df9ed41d65d46f5e37494d
eder24m3za <egmr.49@gmail.com>
2024-02-25 20:43:04 -0600

    Subida

---
+++ storage/framework/views/d49c4c7d324923974759935ff173de4f.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container" style="margin: 100px 85px 85px 85px; padding:12px; border:solid 1px black">
+    <h2>Login</h2>
+        <form action="/login/user" method="POST" id="formLogin">
+        <?php echo csrf_field(); ?>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1" required minlength="8">
+            </div>
+            <?php if($errors->any()): ?>
+        <div class="alert alert-danger">
+            <ul>
+                <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <li><?php echo e($error); ?></li>
+                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+            </ul>
+        </div>
+        <?php endif; ?>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <a href="/register" class="btn btn-link" role="button" aria-pressed="true">Create Account</a>
+        </form>
+    </div>
+</body>
+</html><?php /**PATH C:\Users\egmr9\OneDrive\Documentos\UTT\ING\8\Igmar\practica\resources\views/login.blade.php ENDPATH**/ ?>
